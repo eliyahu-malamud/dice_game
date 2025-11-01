be7ad64c5a7b4358190687cb3c5d435eb9c5c4ad
eliyahu_malamud <elyahumalamud@gmail.com>
2025-11-01 19:10:14 +0200

    test

--- game.py
+++ game.py
@@ -28,3 +28,44 @@ def play_round(main_player, other_player, game_dict) -> int | None:
          else:
              return 1
 
+
+def main_game_loop():
+    game_dict = create_game_dict()
+    flag1 = 0
+    flag2 = 0
+    while True:
+        if flag1 % 2 == 0:
+            a = play_round('player1', 'player2', game_dict)
+            if utils.is_bust(game_dict['player1']):
+                return print('player2 wind because of bust!')
+            elif utils.is_exact_100(game_dict['player1']):
+                return print('player1 wind because of exact 100!')
+            flag1 += 1
+        else:
+            a = play_round('player2', 'player1', game_dict)
+            if utils.is_bust(game_dict['player2']):
+                return print('player1 wind because of bust!')
+            elif utils.is_exact_100(game_dict['player2']):
+                return print('player2 wind because of exact 100!')
+            flag1 += 1
+        if a == 1:
+            flag2 += 1
+        elif a != 1:
+            flag2 = 0
+        if flag2 == 2:
+            if utils.closer_to_target(game_dict['player1'], game_dict['player2']) == 1:
+                print('player1 wind because he is closer to 100')
+                return None
+            elif utils.closer_to_target(game_dict['player1'], game_dict['player2']) == 2:
+                print('player2 wind because he is closer to 100')
+                return None
+            else:
+                return 's'
+
+def secondary_game():
+    if tie_breaker(utils.roll_two_d6) == 1:
+        print('player1 wind because tie breaker!')
+        return None
+    else:
+        print('player2 wind because tie breaker!')
+        return None
